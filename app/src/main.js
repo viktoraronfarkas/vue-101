2c046edd9b527517877e697b82cddacd8afed544
Viktor Farkas <fv1ktor09@gmail.com>
2022-02-17 00:35:05 +0100

    fixes

--- app/src/main.js
+++ app/src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import router from './lib/router';
 import store from './lib/store';
 import './assets/styles/main.css';
-import './lib/calculator';
 
 const app = createApp({});
 app.use(router);
